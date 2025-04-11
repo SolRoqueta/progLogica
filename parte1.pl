b4bc20e8b33f5fb559b985d4859d22031d347e7d
agus-aldunate <agustinaaldunate@gmail.com>
2025-04-11 16:10:24 -0300

    parte 1 mas funcionezs agregadas

--- parte1.pl
+++ parte1.pl
@@ -1,15 +1,18 @@
-es_par(N) :- N mod 2 =:= 0.
-es_impar(N) :- N mod 2 =\= 0.
+es_par(N) :- N mod 2 =:= 0. %predicado para saber si un numero es par
+es_impar(N) :- N mod 2 =\= 0. %predicado para saber si un numero es par
 
+%predicado que retirna una lista con los lares y otra con los impares
 pares_impares_acc([], AcP, AcP, AcI, AcI).
 pares_impares_acc([X|L], AcP, [X|R], AcI, R2) :- es_par(X), pares_impares_acc(L, AcP, R, AcI, R2).
 pares_impares_acc([X|L], AcP, R, AcI, [X|R2]) :- es_impar(X), pares_impares_acc(L, AcP, R, AcI, R2).
 
 pares_impares(L1, L2, L3) :- pares_impares_acc(L1, [], L2, [], L3).
 
+%predicado auxiliar no pertenece
 no_pertenece(_,[]).
 no_pertenece(X,[Y|L]) :- X \= Y, no_pertenece(X,L).
 
+%predicado que se fija si un elemento aparece una unica vez
 unico(X, L) :- select(X, L, R), no_pertenece(X,R).
 
 %Predicado elegir primer elemento
@@ -41,3 +44,22 @@ pares_acum([H|T], Acc, Res) :-
 pares_acum([H|T], Acc, Res) :-
     H mod 2 =\= 0,
     pares_acum(T, Acc, Res).
+
+ordenada([], []).  
+ordenada(L1, L2) :- seleccion(L1, L2).
+
+seleccion(L, [Min|Ordenada]) :- minimoEf(L, Min), eliminar(Min, L, RestoSinMin), ordenada(RestoSinMin, Ordenada).  
+
+minimo_ac([],Ac,Ac). 
+minimo_ac([X|L],Ac, M) :- X < Ac, minimo_ac(L,X,M).
+minimo_ac([X|L],Ac,M) :- X >= Ac, minimo_ac(L,Ac,M).
+
+minimoEf([X|L],M) :- minimo_ac(L,X,M).
+
+
+eliminar(_, [], []). 
+eliminar(X, [X|L], L). 
+eliminar(X, [Y|L], [Y|LRestante]) :- X \= Y, eliminar(X, L, LRestante).  
+
+pertenece(X,[X|_]).
+pertenece(X,[_|L]) :- pertenece(X,L).
