3a057665f9a618feec13c10fc7b9ea758da660ac
sol.roqueta <solroquetasolroqueta@gmail.com>
2025-04-11 16:03:02 -0300

    matriz y predicados sol

--- parte1.pl
+++ parte1.pl
@@ -11,3 +11,33 @@ no_pertenece(_,[]).
 no_pertenece(X,[Y|L]) :- X \= Y, no_pertenece(X,L).
 
 unico(X, L) :- select(X, L, R), no_pertenece(X,R).
+
+%Predicado elegir primer elemento
+elegir_primero(X, L, Res) :-
+    elegir_primero_acum(X, L, [], Res).
+    
+elegir_primero_acum(_, [], Acc, Acc).
+
+elegir_primero_acum(X, [X|T], Acc, Res) :-
+    append(Acc, T, Res).
+
+elegir_primero_acum(X, [H|T], Acc, Res) :-
+    H \= X,
+    append(Acc, [H], Acc1),
+    elegir_primero_acum(X, T, Acc1, Res).
+
+
+%Predicado pares
+pares(L, Res) :-
+    pares_acum(L, [], Res).
+
+pares_acum([], Acc, Acc).
+
+pares_acum([H|T], Acc, Res) :-
+    H mod 2 =:= 0,
+    append(Acc, [H], Acc1),
+    pares_acum(T, Acc1, Res).
+
+pares_acum([H|T], Acc, Res) :-
+    H mod 2 =\= 0,
+    pares_acum(T, Acc, Res).
--- parte2.pl
+++ parte2.pl
@@ -3,3 +3,11 @@ columna([[H|T]|Filas], [H|Columna], [T|Resto]) :- columna(Filas, Columna, Resto)
 
 transpuesta([[]|_],[]).
 transpuesta(M, [C|T]) :- columna(M, C, R), transpuesta(R, T).
+
+columnas([],_).
+columnas([X|Filas],N):-
+    lenght(X,N),
+    columnas(Filas,N).
+
+
+% matriz(N,X):- lenght(X,N), columnas(X,N).
